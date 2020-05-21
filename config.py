6c85259a3b4c0504a5d56d078004bb30fe7011d5
Parth Doshi <parthdoshi97@gmail.com>
2020-05-21 21:46:23 +0530

    Add Python App and Config File
    Added the main Python Application File Crack Detection Using Image Processing and its Config File

---
+++ config.py
@@ -0,0 +1,7 @@
+EMAIL_ADDRESS = #Sender's EMail Address
+PASSWORD = #Sender's Password
+RESULT_EMAIL_ADDRESS = #Receiver's EMail Address
+
+FIREBASE_CONFIG = {
+    #Firebase SDK Configuration Snippet
+  }
