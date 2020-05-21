8d12bbe2c3549589ba239ebe4b473aafbf1c8ffe
Parth Doshi <parthdoshi97@gmail.com>
2020-05-21 21:38:05 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-EMAIL_ADDRESS = #Sender's EMail Address
-PASSWORD = #Sender's Password
-RESULT_EMAIL_ADDRESS = #Receiver's EMail Address
-
-FIREBASE_CONFIG = {
-    #Firebase SDK Configuration Snippet
-  }
